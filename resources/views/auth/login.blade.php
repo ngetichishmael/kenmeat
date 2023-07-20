c36fb1b5cf20634e484f210905a2e1e9c32522fe
Daniel <wisedaniel080@gmail.com>
2023-07-20 12:02:43 +0300

    updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,7 +45,7 @@
                             <div class="mb-1">
                                 <label for="login-email" class="form-label">Email</label>
                                 <input type="text" class="form-control" id="login-email" name="email"
-                                    placeholder="john@example.com" aria-describedby="login-email" tabindex="1"
+                                    placeholder="" aria-describedby="login-email" tabindex="1"
                                     autofocus />
                            
                             </div>
@@ -59,7 +59,7 @@
                                 <div class="input-group input-group-merge form-password-toggle">
                                     <input type="password" class="form-control form-control-merge" id="login-password"
                                         name="password" tabindex="2"
-                                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                        placeholder=""
                                         aria-describedby="login-password" />
                                     <span class="cursor-pointer input-group-text"><i data-feather="eye"></i></span>
                                
