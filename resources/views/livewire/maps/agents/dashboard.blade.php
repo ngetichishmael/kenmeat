c786c944e44adc0d8f1d3ae2e808b041aa575a23
rickxy <omundieric6@gmail.com>
2023-07-20 13:09:46 +0300

    update

--- resources/views/livewire/maps/agents/dashboard.blade.php
+++ resources/views/livewire/maps/agents/dashboard.blade.php
@@ -48,28 +48,82 @@
             height: 800px;
         }
     </style>
-    <button id="toggleButton" class="btn btn-primary">Toggle List</button>
+
+<style>
+    /* Style for the customer list */
+    #customer-list {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    /* Style for each customer item */
+    .customer-item {
+        padding: 5px;
+        padding-left: 10px;
+        border-bottom: 1px solid #ccc;
+        cursor: pointer;
+        transition: background-color 0.3s;
+    }
+
+    .customer-item:hover {
+        background-color: #f2f2f2;
+    }
+
+    /* Style for the customer list container */
+    #customer-list-container {
+        max-height: 900px; /* Adjust this value to set the maximum height of the customer list */
+        max-width: 200px;
+        overflow-y: auto;
+    }
+
+    #toggleButton {
+        float: right;
+        margin-top: 5px;
+    }
+
+    #customer-counter {
+        float: right;
+        margin-top: 4px;
+        margin-right: 10px;
+        font-size: 14px;
+    }
+</style>
+
+<!-- <button id="toggleButton" class="btn btn-primary">Toggle List</button> -->
+<div class="card">
+    <h5 class="card-header">Search Filter</h5>
     <div id="map-container">
         <div id="customer-list-container" style="display: block;">
-            <input id="search-input" type="text" placeholder="Search">
-            <ul id="customer-list">
-                @foreach ($markersByTitle as $title => $markers)
-                    {{-- <li class="customer-title">
-                        <strong>{{ $title }}</strong>
-                    </li> --}}
-                    @foreach ($markers as $marker)
-                        <li class="customer-item" data-latitude="{{ $marker['lat'] }}"
-                            data-longitude="{{ $marker['lng'] }}" data-userCode="{{ $marker['user_code'] }}"
-                            wire:click="plotMarkers('{{ $marker['user_code'] }}')">
-                            <strong>{{ $marker['title'] }}</strong>
-                        </li>
+            <div style="position: relative; background-color: transparent;" class="ml-2">
+                <div class="form-group" style="padding-left: 8px; padding-right: 8px;">
+                    <input id="search-input" class="form-control form-control-sm" type="text" placeholder="Search customer" />
+                </div>
+
+                <br>
+
+                <ul id="customer-list">
+                    @foreach ($markersByTitle as $title => $markers)
+                        {{-- <li class="customer-title">
+                            <strong>{{ $title }}</strong>
+                        </li> --}}
+                        @foreach ($markers as $marker)
+                            <li class="customer-item" data-latitude="{{ $marker['lat'] }}"
+                                data-longitude="{{ $marker['lng'] }}" data-userCode="{{ $marker['user_code'] }}"
+                                wire:click="plotMarkers('{{ $marker['user_code'] }}')">
+                                <strong>{{ $marker['title'] }}</strong>
+                            </li>
+                        @endforeach
                     @endforeach
-                @endforeach
-            </ul>
+                </ul>
+            </div>
             <div id="customer-counter">Total: {{ count($markersByTitle) }}</div>
         </div>
-        <div wire:ignore id="map" style="width:100%; height:800px"></div>
+        <div wire:ignore id="map" style="width: 100%; height: 800px;"></div>
     </div>
+</div>
+
+
     <script>
         const baseUrl = "{{ url('/') }}";
     </script>
