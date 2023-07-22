eb7b153548919fb73385ac35307f28f9d5812e5b
rickxy <omundieric6@gmail.com>
2023-07-22 18:09:55 +0300

    update

--- app/Http/Livewire/Visits/Users/Dashboard.php
+++ app/Http/Livewire/Visits/Users/Dashboard.php
@@ -13,11 +13,20 @@ use Maatwebsite\Excel\Facades\Excel;
 class Dashboard extends Component
 {
     use WithPagination;
+
     public $start;
     public $end;
     protected $paginationTheme = 'bootstrap';
     public $perPage = 10;
     public $search = null;
+    public $selectedDate;
+    public $selectedMonth;
+
+    public function mount()
+    {
+        $this->selectedDate = Carbon::now()->format('Y-m-d');
+        $this->selectedMonth = Carbon::now()->format('Y-m');
+    }
 
     public function render()
     {
@@ -27,44 +36,63 @@ class Dashboard extends Component
     }
 
     public function data()
-{
-    $searchTerm = '%' . $this->search . '%';
-    $query = User::join('customer_checkin', 'users.user_code', '=', 'customer_checkin.user_code')
-        ->whereRaw('customer_checkin.start_time <= customer_checkin.stop_time')
-        ->select(
-            'users.name as name',
-            'users.user_code as user_code',
-            DB::raw('COUNT(customer_checkin.id) as visit_count'),
-            DB::raw('SEC_TO_TIME(AVG(TIME_TO_SEC(TIMEDIFF(customer_checkin.stop_time, customer_checkin.start_time)))) as average_time'),
-            DB::raw('MAX(customer_checkin.start_time) as last_visit_time')
-        )
-        ->where('users.name', 'like', $searchTerm)
-        ->groupBy('users.name');
+    {
+   
 
-    if ($this->start != null) {
-        // If end date is null, set it to the end of the current day
-        $this->end = $this->end ?? Carbon::now()->endOfDay()->format('Y-m-d H:i:s');
+        $searchTerm = '%' . $this->search . '%';
+        $query = User::leftJoin('customer_checkin', function ($join) {
+            $join->on('users.user_code', '=', 'customer_checkin.user_code')
+                ->whereRaw('customer_checkin.start_time <= customer_checkin.stop_time');
+        })
+            ->select(
+                'users.name as name',
+                'users.user_code as user_code',
+                DB::raw('SUM(IF(DATE(customer_checkin.updated_at) = DATE("' . $this->selectedDate . '"), 1, 0)) as today_count'),
+                DB::raw('COUNT(customer_checkin.id) as visit_count'),
+                DB::raw('SEC_TO_TIME(AVG(TIME_TO_SEC(TIMEDIFF(customer_checkin.stop_time, customer_checkin.start_time)))) as average_time'),
+                DB::raw('MAX(customer_checkin.created_at) as last_visit_date') // Use created_at for the last visit date
+            )
+            ->where('users.name', 'like', $searchTerm)
+            ->groupBy('users.name', 'users.user_code')
+            ->havingRaw('visit_count > 0'); // Only include users with completed visits
+    
+        if ($this->selectedMonth != null) {
+            $query->whereYear('customer_checkin.created_at', '=', Carbon::parse($this->selectedMonth)->format('Y'))
+                ->whereMonth('customer_checkin.created_at', '=', Carbon::parse($this->selectedMonth)->format('m'));
+        } else {
+            if ($this->start != null && $this->end != null) {
+                $query->whereBetween('customer_checkin.start_time', [$this->start, $this->end]);
+            } else {
+                $query->whereDate('customer_checkin.updated_at', '=', $this->selectedDate);
+            }
+        }
+    
+        // Order by created_at in descending order to get the latest visit as the first record
+        $query->orderByDesc('customer_checkin.created_at');
+    
+        $visits = $query->paginate($this->perPage);
+    
+        // Set the last_visit_time for each user based on the first record's created_at
+        foreach ($visits as $visit) {
+            $visit->last_visit_time = \Carbon\Carbon::parse($visit->last_visit_date)->format('Y-m-d H:i:s');
+        }
+    
+    
 
-        // Filter by the selected start and end dates (considering both date and time)
-        $query->whereBetween('customer_checkin.start_time', [$this->start . ' 00:00:00', $this->end . ' 23:59:59']);
-    } else {
-        // If start date is not provided, filter by today's date (considering both date and time)
-        $today = Carbon::today()->format('Y-m-d');
-        $query->whereBetween('customer_checkin.start_time', [$today . ' 00:00:00', $today . ' 23:59:59']);
+        return $visits;
     }
-
-    $visits = $query->paginate($this->perPage);
-    return $visits;
-}
-
 
     public function updatedStart()
     {
+        // Clear the selectedMonth when the Date filter changes
+        $this->selectedMonth = null;
         $this->render();
     }
 
     public function updatedEnd()
     {
+        // Clear the selectedMonth when the Date filter changes
+        $this->selectedMonth = null;
         $this->render();
     }
 
