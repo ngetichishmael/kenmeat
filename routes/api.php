2a11770fb4143a43375c1a977ed0fc10e545004b
rickxy <omundieric6@gmail.com>
2023-10-16 07:48:15 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -85,7 +85,7 @@ Route::group(['namespace' => 'Api'], function () {
     // Route::post('checkin/{checkinCode}/add-to-cart',['uses' => 'checkinController@add_to_cart','as' => 'add.to.cart']);
     //Route::post('checkin/{checkinCode}/add-to-cart','checkinController@add_to_cart')->middleware('auth:sanctum');
     //customer checking
-    // Route::post('customer/checkin/session', ['uses' => 'checkinController@create_checkin_session']);
+    // Route::get('customer/checkin/session', ['uses' => 'checkinController@create_checkin_session']);
     // Route::get('customer/{CustomerCode}/checkin', ['uses' => 'checkinController@checkin', 'as' => 'customer.checkin']);
     // Route::get('checkin/{checkinCode}/stock', ['uses' => 'checkinController@stock', 'as' => 'checkin.stock']);
     // Route::get('checkin/{checkinCode}/out', ['uses' => 'checkinController@checkout', 'as' => 'check.out']);
