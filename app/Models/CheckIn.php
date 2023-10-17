10a77d1a0885328e990dde563ecb6d10deb9c3ec
rickxy <omundieric6@gmail.com>
2023-10-17 16:42:18 +0300

    updates

--- app/Models/CheckIn.php
+++ app/Models/CheckIn.php
@@ -8,6 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class CheckIn extends Model
 {
     use HasFactory;
+
     protected $table = "check_ins";
+
     protected $guarded = [''];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
